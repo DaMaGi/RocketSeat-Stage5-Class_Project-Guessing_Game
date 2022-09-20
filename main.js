6124bf58f615384349a6694d52b89566757228fb
Daniel M. Gisi <dmgisi@gmail.com>
2022-09-20 15:34:25 -0300

    Make JS code cleaner

--- main.js
+++ main.js
@@ -2,28 +2,27 @@
 
 let randomNumber = () => Number(Math.random() * 11).toFixed(0);
 
-const inputAttempt = document.getElementById("attemptNum");
-const guessAttemptBtn = document.getElementById("guessBtn");
-const tryAgainBtn = document.getElementById("tryAgain");
+const screen1 = document.getElementsByClassName("main1")[0];
+const screen2 = document.getElementsByClassName("main2")[0];
+const numInputElem = document.getElementById("attemptNum");
 
 let numberToGuess = randomNumber();
 let attemptCounter = 0;
 
-guessAttemptBtn.addEventListener("click", tryToGuess);
-tryAgainBtn.addEventListener("click", restart);
-inputAttempt.addEventListener("keydown", enterGuess);
+numInputElem.addEventListener("keydown", enterGuess);
+document.getElementById("guessBtn").addEventListener("click", tryToGuess);
+document.getElementById("tryAgain").addEventListener("click", restart);
 
 function tryToGuess(event) {
     attemptCounter++;
-    const guess = inputAttempt.value;
-    if (guess == numberToGuess) {
+    if (numInputElem.value == numberToGuess) {
         // Display changes
-        document.getElementsByClassName("main1")[0].style.display = "none";
-        document.getElementsByClassName("main2")[0].style.display = "block";
+        screen1.style.display = "none";
+        screen2.style.display = "block";
         document.getElementById("counter").setHTML(attemptCounter);
         // Event Listener changes
         event.stopPropagation();
-        inputAttempt.removeEventListener("keydown", enterGuess);
+        numInputElem.removeEventListener("keydown", enterGuess);
         document.addEventListener("keydown", enterToTryAgain);
     }
     else {
@@ -35,12 +34,12 @@ function restart(event) {
     attemptCounter = 0;
     numberToGuess = randomNumber();
     // Visual changes
-    document.getElementsByClassName("main2")[0].style.display = "none";
-    document.getElementsByClassName("main1")[0].style.display = "block";
+    screen2.style.display = "none";
+    screen1.style.display = "block";
     // Event Listener changes
     event.stopPropagation();
     document.removeEventListener("keydown", enterToTryAgain);
-    inputAttempt.addEventListener("keydown", enterGuess);
+    numInputElem.addEventListener("keydown", enterGuess);
 }
 
 // Listener functions
