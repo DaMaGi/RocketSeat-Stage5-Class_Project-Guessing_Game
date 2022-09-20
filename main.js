55575b04b687b1470f3612c671b150aa0451818c
Daniel M. Gisi <dmgisi@gmail.com>
2022-09-20 09:39:10 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,61 @@
+"use strict";
+
+let randomNumber = () => Number(Math.random() * 11).toFixed(0);
+
+const inputAttempt = document.getElementById("attemptNum");
+const guessAttemptBtn = document.getElementById("guessBtn");
+const tryAgainBtn = document.getElementById("tryAgain");
+
+let numberToGuess = randomNumber();
+let attemptCounter = 0;
+
+guessAttemptBtn.addEventListener("click", tryToGuess);
+tryAgainBtn.addEventListener("click", restart);
+inputAttempt.addEventListener("keydown", enterGuess);
+
+// debug
+document.getElementsByClassName("main2")[0].style.display = "block";
+
+function tryToGuess(event) {
+    attemptCounter++;
+    const guess = inputAttempt.value;
+    if (guess == numberToGuess) {
+        // Display changes
+        document.getElementsByClassName("main1")[0].style.display = "none";
+        document.getElementsByClassName("main2")[0].style.display = "block";
+        document.getElementById("counter").setHTML(attemptCounter);
+        // Event Listener changes
+        event.stopPropagation();
+        inputAttempt.removeEventListener("keydown", enterGuess);
+        document.addEventListener("keydown", enterToTryAgain);
+    }
+    else {
+        // debug
+        console.log(numberToGuess);
+        return false;
+    }
+}
+
+function restart(event) {
+    attemptCounter = 0;
+    numberToGuess = randomNumber();
+    // Visual changes
+    document.getElementsByClassName("main2")[0].style.display = "none";
+    document.getElementsByClassName("main1")[0].style.display = "block";
+    // Event Listener changes
+    event.stopPropagation();
+    document.removeEventListener("keydown", enterToTryAgain);
+    inputAttempt.addEventListener("keydown", enterGuess);
+}
+
+// Listener functions
+function enterToTryAgain(event) {
+    if (event.key === "Enter") {
+        restart(event);
+    }
+}
+function enterGuess(ev) {
+    if (ev.key === "Enter") { 
+        tryToGuess(ev);
+    }
+}
